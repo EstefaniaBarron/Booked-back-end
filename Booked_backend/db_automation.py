83b45d9413fad3fbc2cdd2cb016b2197e8becd4e
nmohu001 <96404884+nmohu001odu@users.noreply.github.com>
2023-02-19 19:32:18 -0800

    clean up automation script added todo

--- Booked_backend/db_automation.py
+++ Booked_backend/db_automation.py
@@ -1,25 +1,51 @@
-
-
 import sqlite3
 
-try:
-    sqliteConnection = sqlite3.connect('db.sqlite3')
-    cursor = sqliteConnection.cursor()
-    print("Successfully Connected to SQLite")
+sqliteConnection = sqlite3.connect('db.sqlite3')
+cursor = sqliteConnection.cursor()
 
-    sqlite_insert_query =sqlite_insert_query = """INSERT INTO booked_book
-                          (id, title, author_name, binding, price,link) 
-                           VALUES 
-                          (1,'Emma','Jane Austen','paperback',8.99,'www.test.com')"""
-
-    count = cursor.execute(sqlite_insert_query)
-    sqliteConnection.commit()
-    print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+def parseOutput(file):
+    """
+    @todo: 
+        parse output file to format for sqldb and pass into insertBook function 
+    """
+def printSQL(cursor):
+    query="select * from booked_book"
+    
+    cursor.execute(query)
+    
+    rows=cursor.fetchall()
+    
+    # print(rows)
+    for row in rows:
+        for col in row:
+            print(col,end=' ')
+        print()
+   
     cursor.close()
 
-except sqlite3.Error as error:
-    print("Failed to insert data into sqlite table", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+def insertBook():
+    try:
+        #values need to be unique to the book insertions 
+        sqlite_insert_query =sqlite_insert_query = """INSERT INTO booked_book
+                          (id, title, author_name, binding, price,link) 
+                           VALUES 
+                          (3,'Emma','Jane Austen','paperback',8.99,'www.test.com')"""
+
+        count = cursor.execute(sqlite_insert_query)
+        sqliteConnection.commit()
+        print("Insertion complete", cursor.rowcount)
+    
+
+    except sqlite3.Error as error:
+        print("Insertion Error:", error)
+    finally:
+        if sqliteConnection:
+            #print and close
+            printSQL(cursor)
+            print("The SQLite connection is closed")
+
+
+
+
+insertBook()
+
