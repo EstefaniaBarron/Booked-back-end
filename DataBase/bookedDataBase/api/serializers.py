03976cb57f32c2d12738f12c2537868d01cecdde
Estefania Barron <estefaniabarronbarraza@gmail.com>
2023-03-21 18:35:10 -0700

    Added REST API path and serialization for Book data

--- DataBase/bookedDataBase/api/serializers.py
+++ DataBase/bookedDataBase/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from seller.models import bookSellerSite
 from seller.models import Address
+from singleBookData.models import Book
+from singleBookData.models import Listing
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -16,3 +18,17 @@ class SellerSerializer(serializers.ModelSerializer):
     class Meta:
         model = bookSellerSite
         fields = ('StoreName', 'StoreSite', 'locations')
+
+
+class ListingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ('book_store', 'link_url', 'price', 'isbn')
+
+
+class BookSerializer(serializers.ModelSerializer):
+    available_at = ListingsSerializer(many=True)
+
+    class Meta:
+        model = Book
+        fields = ('isbn', 'title', 'author', 'binding', 'available_at')
--- DataBase/bookedDataBase/singleBookData/models.py
+++ DataBase/bookedDataBase/singleBookData/models.py
@@ -1,21 +1,17 @@
 from django.db import models
 from seller.models import bookSellerSite
 
-# Create your models here.  
-# Create your models here.
-#for help: https://realpython.com/django-migrations-a-primer/
+
 class Book(models.Model):
     isbn = models.IntegerField()
-    title= models.CharField(max_length=5000)
-    binding= models.CharField(max_length=5000)
-    author =models.CharField(max_length =5000)
-    
-    
-    
+    title = models.CharField(max_length=5000)
+    binding = models.CharField(max_length=5000)
+    author = models.CharField(max_length=5000)
+
 
 class Listing(models.Model):
-    book_store = models.ForeignKey(bookSellerSite, on_delete=models.CASCADE )
-    link_url =models.URLField(max_length=5000)
-    price =models.FloatField(max_length=300)
+    book_store = models.ForeignKey(
+        bookSellerSite, related_name='available_at', on_delete=models.CASCADE)
+    link_url = models.URLField(max_length=5000)
+    price = models.FloatField(max_length=300)
     isbn = models.ForeignKey(Book, on_delete=models.CASCADE)
-    
