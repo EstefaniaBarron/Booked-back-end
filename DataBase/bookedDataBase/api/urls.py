593b34a3fd46d7924385adf7d22263f4a470e7f6
Estefania Barron <estefaniabarronbarraza@gmail.com>
2023-03-31 13:02:16 -0700

    Added Book endpoint, serializer and view

--- DataBase/bookedDataBase/api/urls.py
+++ DataBase/bookedDataBase/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from seller import views
+from seller.views import SellersView
+from singleBookData.views import BooksView
 
 router = routers.DefaultRouter()
-router.register(r'sellers', views.SellersView)
+router.register(r'sellers', SellersView)
+router.register(r'books', BooksView)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- DataBase/bookedDataBase/singleBookData/views.py
+++ DataBase/bookedDataBase/singleBookData/views.py
@@ -1,3 +1,20 @@
+
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+#from ...api import AddressSerializer
+#from api.serializers import SellerSerializer
+from api.serializers import BookSerializer
+
+#from .models import bookSellerSite
+#from .models import Address
+from .models import Listing
+from .models import Book
+
+
+class BooksView(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
