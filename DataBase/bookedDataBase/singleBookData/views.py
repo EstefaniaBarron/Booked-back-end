1eb80ffab17b18765d515939725792bb98916066
Estefania Barron <estefaniabarronbarraza@gmail.com>
2023-04-11 11:34:09 -0700

    Committing changes from branch rest_api

--- DataBase/bookedDataBase/singleBookData/views.py
+++ DataBase/bookedDataBase/singleBookData/views.py
@@ -9,7 +9,7 @@ from api.serializers import ListingsListSerializer
 from .models import Listing
 from .models import Book
 
-<< << << < HEAD
+
 # Filters through Books model.
 # Overwriting filterset class behavior to return books that contain title and author string vs exact match
 
